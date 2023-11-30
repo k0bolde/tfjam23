50cf67b9e2b949aaad9272dfb32e5058aa78df7c
k0bold <elzilchothe0@gmail.com>
2023-11-30 16:35:34 -0500

    disabled insta quit, added fullscreen to pause info

--- src/Globals.gd
+++ src/Globals.gd
@@ -11,8 +11,8 @@ func _ready():
 
 
 func _unhandled_input(event):
-	if event.is_action_pressed("quit"):
-		get_tree().quit()
+	#if event.is_action_pressed("quit"):
+		#get_tree().quit()
 	if event.is_action_pressed("fullscreen"):
 		if DisplayServer.window_get_mode() == DisplayServer.WINDOW_MODE_WINDOWED:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
